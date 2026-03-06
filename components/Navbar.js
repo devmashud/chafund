679d8b9d59be662718cd575d250e1d750ca8b6e7
devmashud <2345arham@gmail.com>
2026-03-06 21:06:49 +0600

    dropdown add and ui fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
-"use client"
-import { useSession, signIn, signOut } from "next-auth/react"
-import Link from 'next/link'
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { useState } from "react";
 const Navbar = () => {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
+  const [showdropdown, setShowdropdown] = useState(false)
   // if(session) {
   //   return <>
   //     Signed in as {session.user.email} <br/>
@@ -11,10 +13,12 @@ const Navbar = () => {
   // }
 
   return (
-    <nav className='bg-gray-950 border-gray-800 border-b-[0.5px]  text-white flex justify-between items-center px-4
-    h-16'>
-        <div className="logo font-bold text-xl">ChaFund!</div>
-        {/* <ul className='flex justify-between gap-4'>
+    <nav
+      className="bg-gray-950 border-gray-800 border-b-[0.5px]  text-white flex justify-between items-center px-4
+    h-16"
+    >
+      <div className="logo font-bold text-xl">ChaFund!</div>
+      {/* <ul className='flex justify-between gap-4'>
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
@@ -22,25 +26,107 @@ const Navbar = () => {
             <li>Login</li>
         </ul> */}
 
-        <div>
-          {session && <Link href={"/dashboard"}><button className="px-4 py-2 mx-4 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition"
-          >Dashboard
-          </button></Link>}
+      <div>
+        {session && (
+          <>
+            <button
+            onClick={()=>setShowdropdown(!showdropdown)}
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              className="bg-gradient-to-r  from-purple-600 to-blue-500 hover:opacity-90 transition inline-flex items-center justify-center text-white  border border-transparent  focus:ring-1 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-sm px-4 py-2.5 focus:outline-none"
+              type="button"
+            >
+              Welcome {session.user.name}
+              <svg
+                className="w-4 h-4 ms-1.5 -me-0.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 9-7 7-7-7"
+                />
+              </svg>
+            </button>
 
-           {session && <button onClick={()=>signOut()} className="px-4 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition"
-          >Logout
-          </button>}
+            <div
+              id="dropdown"
+              className={`z-10 ${showdropdown?"":"hidden"} absolute bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44`}
+            >
+              <ul
+                className="p-2 text-sm text-body font-medium"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li>
+                  <a
+                    href="#"
+                    className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                  >
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                  >
+                    Earnings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                  >
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
+        {session && (
+          <Link href={"/dashboard"}>
+            <button className="px-4 py-2 mx-4 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition">
+              Dashboard
+            </button>
+          </Link>
+        )}
 
-          {!session &&   <Link href={"/login"}>
-          <button className="px-4 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition"
+        {session && (
+          <button
+            onClick={() => signOut()}
+            className="px-4 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition"
           >
-            Login
+            Logout
           </button>
-          </Link>}
-        
-        </div>
-    </nav>
-  )
-}
+        )}
 
-export default Navbar
+        {!session && (
+          <Link href={"/login"}>
+            <button className="px-4 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-blue-500 hover:opacity-90 transition">
+              Login
+            </button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
